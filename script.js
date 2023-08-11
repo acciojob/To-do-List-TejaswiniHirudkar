15a322781d7cd79fc67bdcb7180f6f6d697d2dab
Tejaswini Hirudkar <121488475+TejaswiniHirudkar@users.noreply.github.com>
2023-08-11 21:59:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-//your code here
+document.addEventListener("DOMContentLoaded", function () {
+  const newTodoInput = document.getElementById("newTodoInput");
+  const addTodoBtn = document.getElementById("addTodoBtn");
+  const todoList = document.getElementById("todoList");
 
+  addTodoBtn.addEventListener("click", function () {
+    const todoText = newTodoInput.value.trim();
+    if (todoText !== "") {
+      const li = document.createElement("li");
+      li.textContent = todoText;
+      todoList.appendChild(li);
+      newTodoInput.value = "";
+    }
+  });
+
+  newTodoInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      addTodoBtn.click();
+    }
+  });
+});
